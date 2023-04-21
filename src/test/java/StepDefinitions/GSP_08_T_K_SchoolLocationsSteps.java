1c0062a0bd70b3f3d8d6179691d3922f4e6f433a
Abdul <elizadeh9797@gmail.com>
2023-04-21 17:53:47 +0400

    Some smal changing ....

--- src/test/java/StepDefinitions/GSP_08_T_K_SchoolLocationsSteps.java
+++ src/test/java/StepDefinitions/GSP_08_T_K_SchoolLocationsSteps.java
@@ -1,77 +1,74 @@
 package StepDefinitions;
 
 import Pages.DialogContent;
-import Pages.LeftNav;
 import Utilities.GWD;
 import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.interactions.Actions;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
 
+import java.time.Duration;
 import java.util.List;
 
 public class GSP_08_T_K_SchoolLocationsSteps {
-
     DialogContent dc = new DialogContent();
-    LeftNav ln = new LeftNav();
-
-    @When("Navigate to Locations")
-    public void navigateToLocations(DataTable items) {
-        List<String> strButtons = items.asList(String.class);
-        for (String strButton: strButtons){
-            ln.clickFunction(ln.getWebElement(strButton));
-        }
-    }
-
-//    @And("Click add button")
-//    public void clickAddButton() {
-//        dc.clickFunction(dc.getWebElement("addButton"));
-//    }
-
-    @And("Enter name, short name, location type and capacity")
-    public void enterNameShortNameLocationTypeAndCapacity(DataTable dt) {
-        List<List<String>> items = dt.asLists(String.class);
-        for (int i = 0; i < items.size(); i++) {
-            dc.sendKeysFunction(dc.getWebElement(items.get(i).get(0)), items.get(i).get(1));
-        }
-        dc.clickFunction(dc.getWebElement("locationType"));
-        Actions actions = new Actions(GWD.getDriver());
-        actions.sendKeys(Keys.DOWN).sendKeys(Keys.ENTER).build().perform();
-    }
-
-    //tr[contains(@class,'remove-background')]//td[2]
-
-//    @And("Click save button")
-//    public void clickSaveButton() {
-//        dc.clickFunction(dc.getWebElement("saveButton"));
-//    }
-
-//    @Then("Success message should be displayed")
-//    public void successMessageShouldBeDisplayed() {
-//        dc.verifyContainsTextFunction(dc.getWebElement("successMsg"), "successfully");
-//    }
-
-//    @Then("Then Already exist should be displayed")
-//    public void thenAlreadyExistShouldBeDisplayed() {
-//        dc.verifyContainsTextFunction(dc.getWebElement("alreadyExist"),"already exists");
-//    }
 
     @And("Click edit button and change name")
-    public void clickEditButtonAndChangeName() {
-        dc.clickFunction(dc.getWebElement("editButton"));
-        dc.sendKeysFunction(dc.getWebElement("nameInput"), "TechnoStudy1");
-        dc.clickFunction(dc.getWebElement("saveButton"));
+    public void clickEditButtonAndChangeName(DataTable dt) {
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("fuse-progress-bar > *"), 0));
+        List<List<String>> items = dt.asLists(String.class);
+
+        for (int i = 0; i < items.size(); i++) {
+            for (int j = 0; j < dc.noSearchButtonForName.size(); j++) {
+                if (dc.noSearchButtonForName.get(j).getAccessibleName().contains(items.get(i).get(0))) {
+                    dc.clickFunction(dc.noSearchButtonForName.get(j));
+                    dc.sendKeysFunction(dc.getWebElement("nameInput"), items.get(i).get(1));
+
+                }
+            }
+        }
     }
 
     @And("Click delete button")
-    public void clickDeleteButton() {
-        dc.clickFunction(dc.getWebElement("deleteImageBtn"));
-        dc.clickFunction(dc.getWebElement("deleteDialogBtn"));
+    public void clickDeleteButton(DataTable dt) {
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("fuse-progress-bar > *"), 0));
+        List<List<String>> items = dt.asLists(String.class);
+
+        for (int i = 0; i < items.size(); i++) {
+            for (int j = 0; j < dc.noSearchButtonForName.size(); j++) {
+                if (dc.noSearchButtonForName.get(j).getAccessibleName().contains(items.get(i).get(0))) {
+                    dc.scrollToElement(dc.NoDeleteButtonsForName.get(j));
+                    dc.clickFunction(dc.NoDeleteButtonsForName.get(j));
+                    dc.clickFunction(dc.getWebElement("deleteDialogBtn"));
+                }
+            }
+        }
     }
 
-//    @Then("No data should be displayed")
-//    public void noDataShouldBeDisplayed() {
-//        dc.verifyContainsTextFunction(dc.getWebElement("noData"), "no data");
-//    }
+    @Then("There is no date with this name")
+    public void thereIsNoDateWithThisName(DataTable dt) {
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.numberOfElementsToBe(By.cssSelector("fuse-progress-bar > *"), 0));
+        List<List<String>> items = dt.asLists(String.class);
+
+        Boolean nodata = false;
+        for (int i = 0; i < items.size(); i++) {
+            for (int j = 0; j < dc.noSearchButtonForName.size(); j++) {
+                if (dc.noSearchButtonForName.get(j).getAccessibleName().contains(items.get(i).get(0))) {
+                    dc.scrollToElement(dc.NoDeleteButtonsForName.get(j));
+                    dc.clickFunction(dc.NoDeleteButtonsForName.get(j));
+                    dc.clickFunction(dc.getWebElement("deleteDialogBtn"));
+                } else {
+                    nodata = true;
+                }
+            }
+        }
+        Assert.assertTrue(nodata, "This function isn't empty");
+
+    }
 }
