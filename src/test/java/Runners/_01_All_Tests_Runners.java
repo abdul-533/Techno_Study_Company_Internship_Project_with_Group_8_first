a815663008de22ad22c429b27df7c46f09e82fa8
Abdul <elizadeh9797@gmail.com>
2023-04-23 23:30:06 +0400

    I update ours framework ....

--- src/test/java/Runners/_01_All_Tests_Runners.java
+++ src/test/java/Runners/_01_All_Tests_Runners.java
@@ -1,7 +1,10 @@
 package Runners;
 
+import Utilities.GWD;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
 @CucumberOptions(
         features = {"src/test/java/FeatureFiles"},
@@ -10,4 +13,10 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"}
 )
 public class _01_All_Tests_Runners extends AbstractTestNGCucumberTests {
+//    @BeforeClass(alwaysRun = true)               (we use this stage for parallel testing)
+//    @Parameters("browserType")
+//    public void beforeClass(String browser) {
+//        GWD.threadBrowserSet(browser);
+//
+//    }
 }
